e7a1d6ffaad20faede2c8085386c2ac58f877897
Naitchi <assasindu39@gmail.com>
2020-08-03 14:35:04 +0200

    Js update

--- public/js/index.js
+++ public/js/index.js
@@ -15,13 +15,13 @@ const getTeddies = async () => {
 };
 
 //faire une fontion 
-const buildALi = (index) =>{
+
+const buildALi = (index,classe) =>{
   const newElement = document.createElement("li");
-  newElement.classList.add("mainSide__searchResults__li");
+  newElement.classList.add(classe);
   if(index%2 == 0){
         newElement.classList.add("--colorBg");
-  }
-  else{
+  } else{
         newElement.classList.add("--darkerColorBg");
   }
   return newElement;
@@ -29,7 +29,7 @@ const buildALi = (index) =>{
 
 const buildADiv = (classe) =>{
   const newElement = document.createElement("div");
-  newElement.classList.add(classe);    
+  newElement.classList.add(classe);
   return newElement;         
 }
 
@@ -40,20 +40,6 @@ const buildAImg = (classe, source) =>{
   return newElement;        
 }
 
-const buildAH2 = (classe, content) =>{
-  const newElement = document.createElement("h2");
-  newElement.classList.add(classe);
-  newElement.textContent = content;    
-  return newElement;         
-}
-
-const buildAH3 = (classe, content) =>{
-  const newElement = document.createElement("h3");
-  newElement.classList.add(classe);
-  newElement.textContent = content;    
-  return newElement;         
-}
-
 const buildAFontAwesomeI = (classe1, classe2) =>{
   const newElement = document.createElement("i");
   newElement.classList.add(classe1);
@@ -61,51 +47,64 @@ const buildAFontAwesomeI = (classe1, classe2) =>{
   return newElement;         
 }
 
-const buildAP = (classe, content) =>{
-  const newElement = document.createElement("p");
+const buildATextContent = (element,classe, content) =>{
+  const newElement = document.createElement(element);
   newElement.classList.add(classe);
   newElement.textContent = content;    
   return newElement;         
 }
 
+//fonction pour les div parents 
+
+const buildAResultDiv = (index, teddy) =>{
+  const actualElement = buildALi(index,"mainSide__searchResults__result");
+  actualElement.appendChild(buildAImg("mainSide__searchResults__result__photo",teddy.imageUrl))
+  actualElement.appendChild(buildATextDiv(teddy));
+  actualElement.appendChild(buildAPAndDDiv(teddy));
+  return actualElement;
+}
+
+const buildATextDiv = (teddy) =>{
+  const actualElement = buildADiv("mainSide__searchResults__result__text");
+  actualElement.appendChild(buildATextContent("h2","mainSide__searchResults__result__text__name",teddy.name));
+  actualElement.appendChild(buildATextContent("p","mainSide__searchResults__result__text__description",teddy.description));
+  return actualElement;
+}
+
+const buildAPAndDDiv = (teddy) =>{
+  const actualElement = buildADiv("mainSide__searchResults__result__D&P");
+  actualElement.appendChild(buildADispoDiv());
+  actualElement.appendChild(buildAPriceDiv(teddy));
+  return actualElement;
+}
+
+const buildADispoDiv = () =>{
+  const actualElement = buildADiv("mainSide__searchResults__result__D&P__Dispo");
+  actualElement.appendChild(buildATextContent("h3","mainSide__searchResults__result__D&P__Dispo__title","Dispo :"));
+  actualElement.appendChild(buildAFontAwesomeI("fas", "fa-check"));
+  return actualElement;
+}
+
+const buildAPriceDiv = (teddy) =>{
+  const actualElement = buildADiv("mainSide__searchResults__result__D&P__Price");
+  actualElement.appendChild(buildATextContent("h3","mainSide__searchResults__result__D&P__Price__title","Prix :"));
+  actualElement.appendChild(buildATextContent("p","mainSide__searchResults__result__D&P__Price__value",teddy.price + " €"));
+  return actualElement;
+}
+
 //FONCTION QUI AJOUTE DU CODE HTML SELON UN RESULTAT
 const buildATeddyDiv = (teddy, startingElement, index) => {
-  startingElement.appendChild(buildALi(index));
-  var selectedElement = startingElement.lastChild;
-  selectedElement.appendChild(buildADiv("mainSide__searchResults__li__result"));
-  const resultElement = selectedElement.lastChild;
-  resultElement.appendChild(buildAImg("mainSide__searchResults__li__result__photo",teddy.imageUrl));
-  selectedElement = resultElement.lastChild;
-  resultElement.appendChild(buildADiv("mainSide__searchResults__li__result__text"));
-  const textElement = resultElement.lastChild;
-  textElement.appendChild(buildAH2("mainSide__searchResults__li__result__name",teddy.name));
-  selectedElement = textElement.lastChild;
-  textElement.appendChild(buildAP("mainSide__searchResults__li__result__description",teddy.description));
-  selectedElement = textElement.lastChild;
-  resultElement.appendChild(buildADiv("mainSide__searchResults__li__result__D&P"));
-  const dandpElement = resultElement.lastChild;
-  dandpElement.appendChild(buildADiv("mainSide__searchResults__li__result__D&P__Dispo"));
-  const dispoElement = dandpElement.lastChild;
-  dispoElement.appendChild(buildAH3("mainSide__searchResults__li__result__D&P__Dispo__title","Dispo :"));
-  selectedElement = dispoElement.lastChild;
-  dispoElement.appendChild(buildAFontAwesomeI("fas", "fa-check"));
-  selectedElement = dispoElement.lastChild;
-  dandpElement.appendChild(buildADiv("mainSide__searchResults__li__result__D&P__Price"));
-  const priceElement = dandpElement.lastChild;
-  priceElement.appendChild(buildAH3("mainSide__searchResults__li__result__D&P__Price__title","Prix :"));
-  selectedElement = priceElement.lastChild;
-  priceElement.appendChild(buildAP("mainSide__searchResults__li__result__D&P__Price__value",teddy.price + " €"));
-  selectedElement = priceElement.lastChild;
+  startingElement.appendChild(buildAResultDiv(index, teddy));
 };
 
-const insertAllTeddies = (elementSearchResult, teddies) => {
-  //FONCTION INSERTION DE TOUT LES RESULTAT GRACE A LA FONCTION BUILDATEDDY
+//FONCTION INSERTION DE TOUT LES RESULTAT GRACE A LA FONCTION BUILDATEDDY
+const insertAllTeddies = (elementSearchResult, teddies) => {  
   console.log(elementSearchResult);
   teddies.map((teddy, index) => buildATeddyDiv(teddy, elementSearchResult,index));
 };
 
+//Mise en place de toutes les fonctions
 getTeddies().then((teddies) => {
-  //Mise en place de toutes les fonctions
   const searchResultsElement = document.getElementById("searchResults");
   insertAllTeddies(searchResultsElement, teddies);
 });
